b37394d0345d26accad45f4fa2422e8a86057f8a
Prajwal-Neupane <prajwalneupane.981@gmail.com>
2026-01-06 20:36:16 +0545

    functions

--- learning/tutorial_1/main.go
+++ learning/tutorial_1/main.go
@@ -6,4 +6,29 @@ import "fmt"
 func main() {
 	fmt.Println("Hello")
 
+	var printValue  string = "Prajwal"
+	printName(printValue)
+
+	var numerator int = 101
+	var denominator int = 20
+
+	var result, remainder = intDivision(numerator, denominator)
+	fmt.Println(result,remainder)
+
+}
+
+func printName(printValue string) {
+	fmt.Println(printValue)
+
+}
+
+func intDivision(numerator int, denominator int) (int , int) {
+	result := numerator/denominator
+	remainder := numerator % denominator
+
+	// Or we can also do this
+	// var remainder int = numerator % denominator
+	// var result int = numerator / denominator
+
+	return result, remainder
 }
