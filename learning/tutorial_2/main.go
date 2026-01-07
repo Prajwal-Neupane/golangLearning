8b0db3fa2a935b18daaeda73e608794d7faacf3d
Prajwal-Neupane <prajwalneupane.981@gmail.com>
2026-01-07 19:40:16 +0545

    slice

--- learning/tutorial_2/main.go
+++ learning/tutorial_2/main.go
@@ -7,10 +7,26 @@ func main() {
 
 	var intArr [5] int64
 
+	// var newArray [3] int32 = [3]int32{1, 2, 3} OR
+
+	newArray := [3]int32{1, 2, 3}
+
+	fmt.Println(&newArray[0])
+
 	// fmt.Printf(intArr[1])
 	fmt.Print(intArr)
 
 	// fmt.Println("Hello")
+
+
+	// Slice
+
+	var intSlice []int32 = []int32{1, 2, 3}
+
+	fmt.Println(intSlice)
+
+	intSlice = append(intSlice, 5)
+	fmt.Println(intSlice)
 
 
 }
