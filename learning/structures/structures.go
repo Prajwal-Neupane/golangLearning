74c2d6d336e0203ddba40dbbc62e6d53231769a5
Prajwal-Neupane <prajwalneupane.981@gmail.com>
2026-01-13 11:52:39 +0545

    structures

--- learning/structures/structures.go
+++ learning/structures/structures.go
@@ -1,19 +1,42 @@
-package structures
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-type gasEngine struct {
+type car struct {
+
+	id string
 	liters uint8
 	kmpl uint16
+	name string
+	status string
+	onProgress bool
+	// buildAt time.Time
+	buildAt string
+}
+
+func (s *car) changeStatus(status string) {
+	s.status = status
 }
 
 
 func main() {
 
-	var myEngine gasEngine = gasEngine{liters: 100, kmpl: 20}
-	// myEngine.kmpl = 100
+	// var myCar car = car{liters: 100, kmpl: 20}
+	// // myEngine.kmpl = 100
 
-	fmt.Println(myEngine.kmpl, myEngine.liters)
+	// fmt.Println(myCar.kmpl, myCar.liters)
+	// var myCar car = car{}
+	// fmt.Println(myCar)
+	now := time.Now()
+
+	myCar2 := car{id: "1", liters: 200, kmpl: 100, name: "BMW", status: "Good", onProgress: true, buildAt: now.Format("2006-01-02")}
+	fmt.Println(myCar2)
+	myCar2.changeStatus("changedStatus")
+
+	fmt.Println(myCar2)
 
 
 }
