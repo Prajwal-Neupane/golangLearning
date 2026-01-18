4d646d54b4cd03194b92c0489964fbe4eff9b487
Prajwal-Neupane <prajwalneupane.981@gmail.com>
2026-01-18 20:01:02 +0545

    files

---
+++ learning/files/files.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+
+	f, err := os.Open("./example.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	fileInfo, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("fileName: ", fileInfo.Name())
+
+
+}
