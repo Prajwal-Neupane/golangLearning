81a3d38062792e94eee62c5fee51324950060af4
Prajwal-Neupane <prajwalneupane.981@gmail.com>
2026-01-18 11:00:42 +0545

    kind of learned interfaces

--- learning/interfaces/interfaces.go
+++ learning/interfaces/interfaces.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
-type payment struct {
 
+type paymenter interface {
+	pay(amount float32)   //interface of paymenter which has pay method which will receive amount variable of float32 type
 }
 
-func (p payment) makePayment(amount float32) {
-	esewaPaymentGateway := esewa{}
-	esewaPaymentGateway.pay(amount)
+type payment struct {
 
+	gateway paymenter
+}
+
+
+
+
+
+func (p payment) makePayment(amount float32) {
+	// esewaPaymentGateway := esewa{}
+	// esewaPaymentGateway.pay(amount)
+	// khaltiPaymentGateway := khalti{}
+
+	p.gateway.pay(amount)
 }
 
 
@@ -17,16 +29,45 @@ type esewa struct {
 
 }
 
+
+
 func (e esewa) pay(amount float32) {
 	// Logic to make payment
 
 	fmt.Println("Making payment through esewa", amount)
 }
 
+type khalti struct {
+
+}
+
+func (k khalti) pay (amount float32) {
+
+	fmt.Println("Making payment through khalti", amount)
+
+}
+
+type paypal struct {
+
+}
+
+func (p paypal) pay(amount float32) {
+	fmt.Println("Making payment through paypal")
+}
+
 
 func main() {
 
-	newPayment := payment{}
-	newPayment.makePayment(100)
+	// newPayment := payment{}
+	// newPayment.makePayment(100)
 
+	// khaltiPaymentGateway := khalti{}
+	paypalPaymentGateway := paypal{}
+
+	newPayment := payment{
+		// gateway: khaltiPaymentGateway,
+		gateway: paypalPaymentGateway,
+	}	// newPayment.makePayment(100)
+
+	newPayment.makePayment(100)
 }
